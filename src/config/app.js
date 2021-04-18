425b61aa225f301b662dffd0ac27d996140028fa
sreekar2307 <bollamsreekhar@gmail.com>
2021-04-18 13:39:57 +0530

    changing api protocol to http

--- src/config/app.js
+++ src/config/app.js
@@ -2,7 +2,7 @@ const constants = {
     'baseURL': "http://localhost:8080/api/v1/"
 }
 if (process.env.NODE_ENV === 'production') {
-    constants['baseURL'] = 'https://ec2-15-206-82-30.ap-south-1.compute.amazonaws.com/api/v1/'
+    constants['baseURL'] = 'http://ec2-15-206-82-30.ap-south-1.compute.amazonaws.com/api/v1/'
 }
 
 export default constants
